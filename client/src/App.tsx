6f57f9f60fed39c640c37d6ceccd00a075e93d72
mushahid <mmushahid@gmail.com>
2024-04-17 17:48:23 +0600

    Blog

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,10 @@ import './App.css'
 import Mushahid from './Mushahid'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-3xl text-red-500">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <Mushahid/>
-      </div>
     </>
   )
 }
--- client/src/Mushahid.tsx
+++ client/src/Mushahid.tsx
@@ -1,7 +1,66 @@
+import React, { useState } from "react";
 
 function Mushahid() {
+
+    const [showBlogs, setShowBlogs] = useState(true);
+    const [showBlogInputBox, setShowBlogInputBox] = useState(false);
+    const [showCreatButton, setShowCreatButton] = useState(true);
+    const [blogTitel, setBlogTitel] = useState("");
+    const [postBody, setPostBody] = useState("");
+
+    const storeTitle = (event) => {
+        setBlogTitel(event.target.value);
+    }
+
+    const storePost = (event) => {
+        setPostBody(event.target.value);
+    }
+
+    function openBlogInputBox() {
+        setShowBlogInputBox(true);
+        setShowCreatButton(false)
+        setShowBlogs(false)
+    }
+
+    function storeBlog() {
+
+        alert(blogTitel);
+        alert(postBody);
+        setPostBody("");
+        setBlogTitel("");
+        setShowBlogInputBox(false);
+        setShowCreatButton(true);
+        setShowBlogs(true)
+
+    }
+
     return (
-        <h1>Hay I am mushahid</h1>
+        <>
+            {
+                showCreatButton &&
+                <button onClick={openBlogInputBox} type="button" className="btn">Write a Blog</button>
+            }
+
+            {
+                showBlogInputBox && (
+                    <div>
+                        <div className="form-group">
+                            <label>Titel</label>
+                            <br />
+                            <input onChange={storeTitle} value={blogTitel} type="text" placeholder="titel" />
+                            <label>Post Body</label>
+                            <br />
+                            <textarea onChange={storePost} value={postBody} placeholder="body..." />
+                        </div>
+                        <button onClick={storeBlog} type="button" className="btn">post</button>
+                    </div>
+                )
+            }
+
+            {
+                showBlogs && <h1>No blog to show @ahsan please create user</h1>
+            }
+        </>
     )
 }
 
