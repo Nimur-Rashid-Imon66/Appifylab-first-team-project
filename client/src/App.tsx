b0505c7cfdecab6f0d3f88ac5dea7b0591635a99
falak50 <83400991+falak50@users.noreply.github.com>
2024-04-17 01:56:52 -1200

    from falak

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           {/* <Route path="/about" element={<About />} /> */}
          </Routes>
       </BrowserRouter>
---
+++ client/src/components/Falak.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Falak = () => {
+    return (
+        <div>
+            <h1>hello from falak</h1>
+        </div>
+    );
+};
+
+export default Falak;
