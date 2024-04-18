1e343495102ea3a0da76c3b8afa4dc1e6a20f5e7
mushahid <mmushahid@gmail.com>
2024-04-18 15:05:05 +0600

    blog-post

--- client/src/Mushahid.tsx
+++ client/src/Mushahid.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardBody, CardFooter, Typography, Button } from "@material-tailwind/react";
 import { Dropdown } from "react-bootstrap";
 
@@ -7,14 +7,13 @@ function Mushahid() {
     let data = JSON.parse(localStorage.getItem('data'));
 
     const [blogs, setBlogs] = useState(data);
-    const [whoIsLoggedIn, setWhoIsLoggedIn] = useState("ahsan");
+    const [whoIsLoggedIn, setWhoIsLoggedIn] = useState("nobody");
 
     const [showBlogs, setShowBlogs] = useState(true);
     const [showBlogInputBox, setShowBlogInputBox] = useState(false);
     const [showCreatButton, setShowCreatButton] = useState(true);
     const [blogTitel, setBlogTitel] = useState("");
     const [postBody, setPostBody] = useState("");
-
 
     if (data == null) {
         const cratingData = {
@@ -49,9 +48,6 @@ function Mushahid() {
 
     function storeBlog() {
 
-        alert(blogTitel);
-        alert(postBody);
-
         const newPost = {
             title: blogTitel,
             body: postBody,
@@ -74,11 +70,54 @@ function Mushahid() {
 
     }
 
+    function remove(indx, author) {
+
+        if (author != whoIsLoggedIn) {
+            alert('Only author can delete the post');
+            return;
+        }
+
+        blogs.blogs.splice(indx, 1);
+        localStorage.setItem('data', JSON.stringify(blogs));
+        setBlogs(JSON.parse(localStorage.getItem('data')));
+    }
+
+    const [openEdit, setOpenEdit] = useState(false);
+    const [editId, setEditId] = useState(null);
+    const [editValue, setEditValue] = useState('');
+
+    function edit(indx, author, value) {
+        if (author != whoIsLoggedIn) {
+            alert('Only author can edit the post');
+            return;
+        }
+        else {
+            setOpenEdit(true);
+            setEditId(indx);
+            setEditValue(value)
+        }
+    }
+
+    const storeEdit = (event) => {
+        setEditValue(event.target.value);
+    }
+
+    function update() {
+
+        blogs.blogs[editId]['body'] = editValue;
+        localStorage.setItem('data', JSON.stringify(blogs));
+        setBlogs(JSON.parse(localStorage.getItem('data')));
+
+        setOpenEdit(false);
+        setEditId(null);
+        setEditValue('')
+    }
+
     return (
         <>
             <div>
                 {
-                    showCreatButton && (whoIsLoggedIn != 'nobody') &&
+                    showCreatButton && (whoIsLoggedIn != 'nobody') && (whoIsLoggedIn != 'nobody') &&
                     <button onClick={openBlogInputBox} type="button" className="btn">Write a Blog</button>
                 }
             </div>
@@ -100,20 +139,35 @@ function Mushahid() {
             }
             <div>
                 {
-                    showBlogs && blogs.blogs.map((val) => (
+                    showBlogs && blogs.blogs.map((val, indx) => (
                         <>
                             <br />
                             {/* <b>{val.title} {" "} </b>
                             <small>(author: {val.author})</small> */}
-                            
+
 
                             <div style={{ border: '1px solid gray', margin: '8px', padding: '8px' }}>
-                                <b>{val.title} {" "} </b>
-                                <small>(author: {val.author})</small>
-                                
-                                <p>{val.body}</p>
-                                <button style={{ marginRight: '5px', padding: '5px 10px', backgroundColor: '#ffc107', color: 'black', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Edit</button>
-                                <button style={{ padding: '5px 10px', backgroundColor: '#f44336', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Delete</button>
+                                {
+                                    (editId != indx) && (<>
+                                        <b>{val.title} {" "} </b>
+                                        <small>(author: {val.author})</small>
+
+                                        <p>{val.body}</p>
+                                        <button onClick={() => edit(indx, val.author, val.body)} style={{ marginRight: '5px', padding: '5px 10px', backgroundColor: '#ffc107', color: 'black', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Edit</button>
+                                        <button onClick={() => remove(indx, val.author)} style={{ padding: '5px 10px', backgroundColor: '#f44336', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Delete</button>
+
+                                    </>
+                                    )
+                                }
+                                {
+                                    openEdit && (editId == indx) && (<>
+                                        <div className="form-group">
+                                            <textarea onChange={storeEdit} value={editValue} placeholder="body..." />
+                                        </div>
+                                        <button onClick={update} type="button" className="btn">update</button>
+                                    </>
+                                    )
+                                }
                             </div>
                         </>
                     ))
