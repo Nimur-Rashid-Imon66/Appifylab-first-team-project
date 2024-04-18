bc2e91472d9f395a615789eed209049581f8bdf4
Ahsan <ahsanhabib462@gmail.com>
2024-04-18 11:22:03 +0600

    Add Online User

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,12 +1,20 @@
 import { useContext } from "react";
 import { OnlineUserContext } from "./App";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { currentLoginUser, _ } = useContext(OnlineUserContext);
+  const { currentLoginUser, setCurrentLoginUser } =
+    useContext(OnlineUserContext);
+  const navigate = useNavigate();
+  const logingout = () => {
+    setCurrentLoginUser(-1);
+    navigate("/login");
+  };
   return (
     <>
       <h1>Hello I am user Number</h1>
       {currentLoginUser}
+      <button onClick={logingout}>Log Out</button>
     </>
   );
 };
