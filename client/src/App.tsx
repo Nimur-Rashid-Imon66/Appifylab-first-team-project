bf6b587a00680c87df66ad4cc1019ae39e232b3a
Ahsan <ahsanhabib462@gmail.com>
2024-04-20 16:28:38 +0600

    c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,32 +29,6 @@ const App: React.FC = () => {
     console.log(localhostUserData);
   };
 
-
-interface UserData {
-  username: string;
-  password: string;
-}
-
-// Create the context
-export const OnlineUserContext = createContext("");
-
-const App: React.FC = () => {
-  const [currentLoginUser, setCurrentLoginUser] = useState(0);
-  const getdata: UserData[] = JSON.parse(
-    localStorage.getItem("localhostUserData") || "[]"
-  );
-  const localhostUserData: UserData[] = getdata;
-
-  const addUser = (username: string, password: string) => {
-    localhostUserData.push({ username, password });
-    // Storing data
-    localStorage.setItem(
-      "localhostUserData",
-      JSON.stringify(localhostUserData)
-    );
-    console.log(localhostUserData);
-    console.log("localhostUserData");
-  };
   return (
     <>
       <OnlineUserContext.Provider
