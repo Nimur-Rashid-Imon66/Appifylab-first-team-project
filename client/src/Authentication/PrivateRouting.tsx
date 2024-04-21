fa3e3b165d085a30790de7d814ec77337b70af96
Ahsan <ahsanhabib462@gmail.com>
2024-04-21 16:37:11 +0600

    Complete Authentication

--- client/src/Authentication/PrivateRouting.tsx
+++ client/src/Authentication/PrivateRouting.tsx
@@ -1,42 +1,11 @@
-// App.js
+import React, { useContext } from "react";
+import { OnlineUserContext } from "../App";
+import { Navigate, Outlet } from "react-router-dom";
 
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+function PrivateRouting() {
+  const { currentLoginUser, setCurrentLoginUser } =
+    useContext(OnlineUserContext);
+  return currentLoginUser.id != -1 ? <Outlet /> : <Navigate to={"/login"} />;
+}
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { isLoggedIn } = useAuth();
-
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        )
-      }
-    />
-  );
-};
-
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <PrivateRoute path="/dashboard" component={Dashboard} />
-      </Switch>
-    </Router>
-  );
-};
-
-export default App;
+export default PrivateRouting;
